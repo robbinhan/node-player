9f87d404b2205b723df54713029c0d77d97e06ed
韩斌 <hanb@kingnet.com>
2018-12-25 11:02:06 +0800

    upgrade

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
-// require('longjohn');
+var fs = require("fs");
+var path = require("path");
+var Koa = require("koa");
+var Router = require("koa-router");
+var serve = require("koa-static");
+const render = require("koa-ejs");
 
-var fs = require('fs');
-var path = require('path');
-var koa = require('koa');
-var route = require('koa-route');
-var views = require('co-views');
-var serve = require('koa-static');
-var app = koa();
+var app = new Koa();
+var router = new Router();
 
-var render = views(__dirname, { ext: 'ejs' });
-app.use(serve('.'));
+render(app, {
+  root: path.join(__dirname, "views"),
+  layout: false,
+  viewExt: "ejs",
+  cache: false,
+  debug: false
+});
 
-app.use(route.get('/', function *(){
-  this.body = yield render('index');
-}));
+app.use(serve("."));
+
+router.get("/", async (ctx, next) => {
+  await ctx.render("index");
+});
 
 /**
  * 获取播放列表
  */
-app.use(route.get('/api/list', list));
+router.get("/api/list", async (ctx, next) => {
+  playList = {};
+  walkInto("./music/", [".mp3"], (err, data) => {
+    playList = data;
+  });
+  ctx.body = playList;
+});
 
-function *list() {
-  playList = {}
-  walkInto('./music/',['.mp3'],function(err,data){
-      playList = data
-  })
-  this.response.status= 200
-  this.response.body= playList;
-}
+app.use(router.routes());
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("start server on 3000");
+});
 
 /**
  * 遍历目录
@@ -39,34 +47,34 @@ app.listen(3000);
  * @return {[type]}          [description]
  */
 function walkInto(dir, excludes, back) {
-    var result = [];
-    var files = fs.readdirSync(dir);
-    files = files.filter(function(value){
-      var extName = path.extname(value)
-      if (value[0] == '.') {
-        return false;
-      }
-      /**
-       * 没有扩展名的认为是目录,保留
-       */
-      if (!extName) {
-        return true;
-      }
-      return (excludes.indexOf(extName) == 0);
-    });
-    var pending = files.length;
-    if (!pending) return back(null, result);
-    files.forEach(function(file){
-      var stats = fs.statSync(dir + '/' + file);
-      if (stats.isFile()) {
-          result.push({path:'/' + dir + '/' + file,name:file})
-          if (!--pending) back(null, result);
-      }
-      if (stats.isDirectory()) {
-          walkInto(dir + '/' + file, excludes, function(err, res){
-              result = result.concat(res);
-              if (!--pending) back(null, result);
-          })
-      }
-    })
+  var result = [];
+  var files = fs.readdirSync(dir);
+  files = files.filter(function(value) {
+    var extName = path.extname(value);
+    if (value[0] == ".") {
+      return false;
+    }
+    /**
+     * 没有扩展名的认为是目录,保留
+     */
+    if (!extName) {
+      return true;
+    }
+    return excludes.indexOf(extName) == 0;
+  });
+  var pending = files.length;
+  if (!pending) return back(null, result);
+  files.forEach(function(file) {
+    var stats = fs.statSync(dir + "/" + file);
+    if (stats.isFile()) {
+      result.push({ path: "/" + dir + "/" + file, name: file });
+      if (!--pending) back(null, result);
+    }
+    if (stats.isDirectory()) {
+      walkInto(dir + "/" + file, excludes, function(err, res) {
+        result = result.concat(res);
+        if (!--pending) back(null, result);
+      });
+    }
+  });
 }
